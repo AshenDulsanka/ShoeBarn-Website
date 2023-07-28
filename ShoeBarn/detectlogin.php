88715dda92480e92be5404179a8c3c2c92ff02f8
Ashen Abeysekara <ashendul@gmail.com>
2023-07-29 03:24:30 +0530

    Revert "Add files via upload"

--- ShoeBarn/detectlogin.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (isset($_SESSION['userid']))
-{
-echo "<p style='float: right'><i><b>Account: ".$_SESSION['fname']." ".$_SESSION['sname']." |"." User Type: ".$_SESSION['usertype']."</b></i></p>";
-}
-?>
